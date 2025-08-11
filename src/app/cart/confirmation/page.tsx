c8d48f5ee63f1d77662b28e044ba5d3870b9bfe5
WagnerK4uan <wkmachado08@gmail.com>
2025-08-11 11:37:09 -0300

    feat: ensure order can be created

--- src/app/cart/confirmation/page.tsx
+++ src/app/cart/confirmation/page.tsx
@@ -1,4 +1,3 @@
-import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
@@ -12,11 +11,11 @@ import {
   CardTitle,
 } from "@/src/components/ui/card";
 import { db } from "@/src/db";
-import { shippingAddressTable } from "@/src/db/schema";
 import { auth } from "@/src/lib/auth";
 
 import { formatAddress } from "../helpers/address";
 import CartSummary from "../identification/components/cart-summary";
+import FinishOrderButton from "./components/finish-order-button";
 
 const ConfirmationPage = async () => {
   const session = await auth.api.getSession({
@@ -64,9 +63,7 @@ const ConfirmationPage = async () => {
                 <p className="text-sm">{formatAddress(cart.shippingAddress)}</p>
               </CardContent>
             </Card>
-            <Button className="w-full rounded-full" size="lg">
-              Finalizar compra
-            </Button>
+            <FinishOrderButton />
           </CardContent>
         </Card>
         <CartSummary
